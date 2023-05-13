e01145f5db2e1a74418d5e35dc9d7127856bbc9c
pq <1132408017@qq.com>
2023-05-13 19:14:44 +0800

    分类视频

--- src/main/java/com/yali/vilivili/mapper/CollectionMapper.java
+++ src/main/java/com/yali/vilivili/mapper/CollectionMapper.java
@@ -1,6 +1,5 @@
 package com.yali.vilivili.mapper;
 
-<<<<<<< HEAD
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yali.vilivili.model.entity.CollectionEntity;
@@ -10,16 +9,4 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface CollectionMapper extends BaseMapper<CollectionEntity> {
 
-=======
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yali.vilivili.model.entity.CollectionEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-/**
- * @author xujh
- * @date 13/5/2023
- */
-@Mapper
-public interface CollectionMapper extends BaseMapper<CollectionEntity> {
->>>>>>> 3619644eefe951646dda96a02a8c3f553e6f36b9
 }
