6e924937a816d86d128d1741101c6e4f5d48d01e
yujie <2901570623@qq.com>
2023-05-13 16:58:46 +0800

    bug

--- src/main/java/com/yali/vilivili/mapper/VideosEntityMapper.java
+++ src/main/java/com/yali/vilivili/mapper/VideosEntityMapper.java
@@ -4,7 +4,9 @@ package com.yali.vilivili.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yali.vilivili.model.entity.VideosEntity;
 import com.yali.vilivili.model.entity.VideosInfoEntity;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface VideosEntityMapper extends BaseMapper<VideosEntity> {
 
 }
--- src/main/java/com/yali/vilivili/mapper/VideosInfoEntityMapper.java
+++ src/main/java/com/yali/vilivili/mapper/VideosInfoEntityMapper.java
@@ -4,6 +4,9 @@ package com.yali.vilivili.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yali.vilivili.model.entity.VideosEntity;
 import com.yali.vilivili.model.entity.VideosInfoEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 
 public interface VideosInfoEntityMapper extends BaseMapper<VideosInfoEntity> {
 }
