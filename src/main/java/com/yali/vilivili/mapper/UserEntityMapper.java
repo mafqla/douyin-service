c35d81c0e1875b1a367f29e3e5417f82bba791f3
pq <1132408017@qq.com>
2023-05-13 18:06:55 +0800

    分类视频

---
+++ src/main/java/com/yali/vilivili/mapper/UserEntityMapper.java
@@ -0,0 +1,12 @@
+package com.yali.vilivili.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yali.vilivili.model.entity.UserEntity;
+import com.yali.vilivili.model.entity.VideosEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserEntityMapper extends BaseMapper<UserEntity> {
+
+}
