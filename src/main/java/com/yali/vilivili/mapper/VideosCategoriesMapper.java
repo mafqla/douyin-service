7dcd07c9289824cab0564a44963658b2723c0efe
pq <1132408017@qq.com>
2023-05-13 22:39:29 +0800

    1

---
+++ src/main/java/com/yali/vilivili/mapper/VideosCategoriesMapper.java
@@ -0,0 +1,12 @@
+package com.yali.vilivili.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yali.vilivili.model.entity.CollectionEntity;
+import com.yali.vilivili.model.entity.VideosCategoriesEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface VideosCategoriesMapper extends BaseMapper<VideosCategoriesEntity> {
+
+}
